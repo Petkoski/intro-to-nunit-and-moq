deaf1222f50228074ab61af0d3a8557c1e2e466a
Jovan Petkoski <petkoski15@hotmail.com>
2019-08-16 11:21:23 +0200

    SetUp & TearDown

--- TestDemo.IntegrationTests/StringCalculatorIntegrationTests.cs
+++ TestDemo.IntegrationTests/StringCalculatorIntegrationTests.cs
@@ -11,14 +11,28 @@ namespace TestDemo.IntegrationTests
     [TestFixture]
     public class StringCalculatorIntegrationTests
     {
+        private string _filepath = @"E:\CSharp\Training\IntroToNUnitAndMoq\test.txt";
+
+        [SetUp]
+        public void Setup()
+        {
+            if (File.Exists(_filepath))
+                File.Delete(_filepath);
+        }
+
         [Test]
         public void Add_ResultIsPrime_CreatesAFile()
         {
-            string filePath = @"E:\CSharp\Training\IntroToNUnitAndMoq\test.txt";
-            FileStore store = new FileStore(filePath);
+            FileStore store = new FileStore(_filepath);
             StringCalculator calc = new StringCalculator(store);
             var result = calc.Add("3,4");
-            Assert.IsTrue(File.Exists(filePath)); //Testing the existance of the file
+            Assert.IsTrue(File.Exists(_filepath)); //Testing the existance of the file
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Setup();
         }
     }
 }
