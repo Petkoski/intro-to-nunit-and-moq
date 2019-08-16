4ab8878a725e1f5c1125b4fa2c1d4f937a36b7d9
Jovan Petkoski <petkoski15@hotmail.com>
2019-08-16 11:27:26 +0200

    Integration test that checks if correct result is written to file

--- TestDemo.IntegrationTests/StringCalculatorIntegrationTests.cs
+++ TestDemo.IntegrationTests/StringCalculatorIntegrationTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System.IO;
+using System.Linq;
 
 /**
  * Example: Client specifies how they want their results to be saved (in the case of prime numbers) - to text files on disk
@@ -27,6 +28,20 @@ namespace TestDemo.IntegrationTests
             StringCalculator calc = new StringCalculator(store);
             var result = calc.Add("3,4");
             Assert.IsTrue(File.Exists(_filepath)); //Testing the existance of the file
+        }
+
+        [Test]
+        [TestCase("33,4", "37")]
+        [TestCase("10,1", "11")]
+        public void Add_NumberIsPrime_WritesCorrectResultToFile(string input, string expectedResult)
+        {
+            FileStore store = new FileStore(_filepath);
+            StringCalculator calc = new StringCalculator(store);
+            var result = calc.Add(input);
+
+            var storedResult = File.ReadLines(_filepath).FirstOrDefault();
+
+            Assert.That(storedResult, Is.EqualTo(expectedResult));
         }
 
         [TearDown]
