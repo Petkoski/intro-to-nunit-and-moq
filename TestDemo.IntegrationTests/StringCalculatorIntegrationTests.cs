0860b36c0c0103d4a6aa2ca7b1fe8fe151304e23
Jovan Petkoski <petkoski15@hotmail.com>
2019-08-16 11:08:46 +0200

    Testing the existance of a file (integration test)

---
+++ TestDemo.IntegrationTests/StringCalculatorIntegrationTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+/**
+ * Example: Client specifies how they want their results to be saved (in the case of prime numbers) - to text files on disk
+ * Integration tests - rely on some infrastructure (maybe a web service, database or in this case - file system)
+ */
+
+namespace TestDemo.IntegrationTests
+{
+    [TestFixture]
+    public class StringCalculatorIntegrationTests
+    {
+        [Test]
+        public void Add_ResultIsPrime_CreatesAFile()
+        {
+            string filePath = @"E:\CSharp\Training\IntroToNUnitAndMoq\test.txt";
+            FileStore store = new FileStore(filePath);
+            StringCalculator calc = new StringCalculator(store);
+            var result = calc.Add("3,4");
+            Assert.IsTrue(File.Exists(filePath)); //Testing the existance of the file
+        }
+    }
+}
---
+++ TestDemo/FileStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDemo
+{
+    public class FileStore : IStore
+    {
+        private readonly string _filePath;
+
+        public FileStore(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public int result { get; set; }
+
+        public void Save(int result)
+        {
+            using (var writer = File.CreateText(_filePath))
+            {
+                writer.WriteLine(result);
+            }
+        }
+    }
+}
